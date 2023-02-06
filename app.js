4d62b91601d25a9cf92b9079a16e8c3104cd1d6a
Abhishek Ranjan <abhishekrjnn@gmail.com>
2023-02-06 19:02:37 +0530

    chained route handlers

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB", { useNewUrlParser: true });
 
 const articleSchema = {
     title: String,
@@ -17,46 +17,46 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-    Article.find({}, function(err, foundArticles){
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.send(foundArticles);
-        }
+
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find({}, function (err, foundArticles) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.send(foundArticles);
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.send("saved to database");
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany({}, function (err) {
+            if (!err) {
+                res.send("successfully deleted all articles!");
+            }
+            else {
+                res.send(err);
+            }
+        });
     });
-});
-
-app.post("/articles", function(req, res){
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newArticle.save(function(err){
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.send("saved to database");
-        }
-    });  
-});
-
-app.delete("/articles", function(req,res){
-    Article.deleteMany({},function(err){
-        if(!err){
-            res.send("successfully deleted all articles!");
-        }
-        else{
-            res.send(err);
-        }
-    });
-});
 
 
 
 
-app.listen(3000, function(req, res){
+app.listen(3000, function (req, res) {
     console.log("Server started on port 3000");
 })
